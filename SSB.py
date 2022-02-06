1554b66de85ed4a149e50768a2adcb98fff64857
Sarfraz Ahmed Shar <86257930+Sarfraz-Baloch@users.noreply.github.com>
2022-02-06 12:49:13 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -7,7 +7,7 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from ssb import menu
-    menu()
+    from ssb import ran
+    ran()
 elif bit == '32bit':
     print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
